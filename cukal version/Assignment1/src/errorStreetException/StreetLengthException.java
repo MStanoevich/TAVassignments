53565e53931de88409beb4e628912d9947512a1a
Unknown <guschuma@student.gu.se>
2018-02-02 19:06:50 +0100

    merge conflicts

--- cukal version/Assignment1/src/errorStreetException/StreetLengthException.java
+++ cukal version/Assignment1/src/errorStreetException/StreetLengthException.java
@@ -1,26 +1,7 @@
 package errorStreetException;
 
-<<<<<<< Updated upstream
-public class StreetLengthException extends Exception{
-	
-		
-		public StreetLengthException(String message){
-			super(message);
-		};
-
-		public StreetLengthException() throws StreetLengthException{	
-			throw new StreetLengthException("No road left for the car to move");
-		}
-		
-		public String toString(){	
-			return "No road left for the car to move";
-			
-		}
-	}
-=======
 /*Class created to generate the street length exception when the car is out of range */
 public class StreetLengthException extends Exception {
->>>>>>> Stashed changes
 
 	public StreetLengthException(String message) {
 		super(message);
--- cukal version/Assignment1/src/main/Main.java
+++ cukal version/Assignment1/src/main/Main.java
@@ -8,25 +8,7 @@ public class Main {
 		Car car = new Car();
 		 int lane = car.whereIs().getCarLane();
 		
-<<<<<<< Updated upstream
-		
-		 UltrasoundSensor r1 = new UltrasoundSensor();
-		 UltrasoundSensor r2 = new UltrasoundSensor();
-		 UltrasoundSensor r3 = new UltrasoundSensor();
-		 Lidar l = new Lidar();
-		 //testing purposes
-		 r1.setSensorValue(-1);
-		 r2.setSensorValue(-1);
-		 r3.setSensorValue(-1);
-		 l.setLidarValue(1);
-		 
-		 
-		 boolean res = car.leftLaneDetect(r1, r2, r3, l);
-		 System.out.println(res);
-=======
 		 System.out.println(lane);
->>>>>>> Stashed changes
-		 
 		 
 	}
 
